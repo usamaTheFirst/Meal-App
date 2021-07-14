4b5d71e8d6f1d25c24646ec83751d39326ff617a
usamaTheFirst <usamafiaz1453@gmail.com>
2021-07-14 22:05:44 +0500

    initial

---
+++ lib/Widgets/category_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  const CategoryItem({Key key, @required this.title, @required this.color})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: Text(title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [color.withOpacity(.7), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
---
+++ lib/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'Widgets/category_item.dart';
+import 'dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Khana App'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: GridView(
+          padding: EdgeInsets.all(20),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: DUMMY_CATEGORIES.map((item) {
+            return CategoryItem(title: item.title, color: item.color);
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
